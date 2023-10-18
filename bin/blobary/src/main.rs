8fcf1322f1745578c61eacf0c5b975febce69ec1
Arto Bendiken <arto@bendiken.net>
2023-10-18 04:14:17 +0400

    Implement the CLI skeleton.

--- bin/blobary/src/main.rs
+++ bin/blobary/src/main.rs
@@ -1,3 +1,185 @@
 // This is free and unencumbered software released into the public domain.
 
-pub fn main() {}
+mod config;
+mod sysexits;
+
+use crate::sysexits::{exit, Sysexits};
+use clap::{Parser, Subcommand};
+use dotenvy::dotenv;
+use shadow_rs::shadow;
+use std::path::PathBuf;
+
+shadow!(build);
+
+/// Blobary command-line interface (CLI)
+#[derive(Parser, Debug)]
+#[command(name = "Blobary", about)]
+#[command(arg_required_else_help = true)]
+struct Options {
+    /// Enable debugging output
+    #[clap(short = 'd', long, value_parser)]
+    debug: bool,
+
+    /// Show license information
+    #[clap(long, value_parser)]
+    license: bool,
+
+    // Enable verbose output
+    #[clap(short = 'v', long, value_parser)]
+    verbose: bool,
+
+    /// Print version information
+    #[clap(short = 'V', long, value_parser)]
+    version: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Show the current configuration
+    Config {},
+    /// Print out the hash of a file
+    #[clap(aliases = &["id", "identify"])]
+    Hash { path: PathBuf },
+    /// Initialize `$HOME/.config/blobary`
+    Init {},
+    /// Check the repository integrity
+    Check {},
+    /// List blobs in the repository
+    #[clap(alias = "ls")]
+    List {},
+    /// Add a file to the repository
+    Add { path: PathBuf },
+    /// Put text into the repository
+    Put { text: String },
+    /// Fetch a blob by its hash
+    #[clap(alias = "cat")]
+    Get { id: String },
+    /// Remove a blob by its hash
+    #[clap(aliases = &["rm", "del", "delete"])]
+    Remove { id: String },
+    /// Pull blobs from another repository
+    Pull { url: String },
+    /// Push blobs to another repository
+    Push { url: String },
+    /// Sync blobs with another repository
+    Sync { url: String },
+    /// TODO
+    Import { path: PathBuf },
+    /// TODO
+    Export { path: PathBuf },
+}
+
+pub fn main() {
+    // Load environment variables from `.env`:
+    dotenv().ok();
+
+    // Expand wildcards and @argfiles:
+    let args = wild::args_os();
+    let args = argfile::expand_args_from(args, argfile::parse_fromfile, argfile::PREFIX).unwrap();
+
+    // Parse command-line options:
+    let options = Options::parse_from(args);
+
+    if options.version {
+        exit(version());
+    }
+
+    if options.license {
+        exit(license());
+    }
+
+    if options.verbose || options.debug {
+        // TODO: configure tracing
+    }
+
+    let result = match &options.command.expect("command is required") {
+        Commands::Config {} => config(),
+        Commands::Hash { path } => hash(path),
+        Commands::Init {} => init(),
+        Commands::Check {} => check(),
+        Commands::List {} => list(),
+        Commands::Add { path } => add(path),
+        Commands::Put { text } => put(text),
+        Commands::Get { id } => get(id),
+        Commands::Remove { id } => remove(id),
+        Commands::Pull { url } => pull(url),
+        Commands::Push { url } => push(url),
+        Commands::Sync { url } => sync(url),
+        Commands::Import { path } => import(path),
+        Commands::Export { path } => export(path),
+    };
+
+    exit(result);
+}
+
+fn version() -> Sysexits {
+    let (date, _) = build::BUILD_TIME_3339.split_once('T').unwrap();
+    let version_string = format!("{} ({} {})", build::PKG_VERSION, date, build::SHORT_COMMIT,);
+    println!("Blobary {}", version_string);
+    Sysexits::EX_OK
+}
+
+fn license() -> Sysexits {
+    let license = include_str!("../../../UNLICENSE");
+    println!("{}", license.trim_end());
+    Sysexits::EX_OK
+}
+
+fn config() -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn hash(_path: &PathBuf) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn init() -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn check() -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn list() -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn add(_path: &PathBuf) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn put(_text: &String) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn get(_id: &String) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn remove(_id: &String) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn pull(_url: &String) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn push(_url: &String) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn sync(_url: &String) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn import(_path: &PathBuf) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
+
+fn export(_path: &PathBuf) -> Sysexits {
+    Sysexits::EX_OK // TODO
+}
---
+++ bin/blobary/src/sysexits.rs
@@ -0,0 +1,33 @@
+// This is free and unencumbered software released into the public domain.
+
+#[allow(non_camel_case_types, dead_code)]
+pub enum Sysexits {
+    EX_OK = 0,
+    EX_USAGE = 64,
+    EX_DATAERR = 65,
+    EX_NOINPUT = 66,
+    EX_NOUSER = 67,
+    EX_NOHOST = 68,
+    EX_UNAVAILABLE = 69,
+    EX_SOFTWARE = 70,
+    EX_OSERR = 71,
+    EX_OSFILE = 72,
+    EX_CANTCREAT = 73,
+    EX_IOERR = 74,
+    EX_TEMPFAIL = 75,
+    EX_PROTOCOL = 76,
+    EX_NOPERM = 77,
+    EX_CONFIG = 78,
+}
+
+pub fn exit(code: Sysexits) -> ! {
+    std::process::exit(code as i32);
+}
+
+#[allow(unused_macros)]
+macro_rules! abort {
+    ($code:expr, $($t:tt)*) => {{
+        eprintln!($($t)*);
+        exit($code)
+    }};
+}
